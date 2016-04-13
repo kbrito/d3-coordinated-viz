73b4ca33619af1d3468b5d42debf8a52a47af61b
kbrito <airnomadkai@gmail.com>
2016-04-13 02:23:42 -0500

    design

--- js/main.js
+++ js/main.js
@@ -19,7 +19,7 @@ function setMap(){
     //create Albers equal area conic projection centered on France
     var projection = d3.geo.albers()
     .rotate([108, 0])
-    .center([-3.64, 33.6])
+    .center([-3.64, 34.0])
     .parallels([29.5, 45.5])
     .scale(3000)
     .translate([width / 2, height / 2])
@@ -45,10 +45,35 @@ function setMap(){
             .attr("class", "COUNTY")
             .attr("d", path);
 
+        //variables for data join
+        var attrArray = ["varA", "varB", "varC", "varD", "varE"];
+
+        //loop through csv to assign each set of csv attribute values to geojson region
+        for (var i=0; i<csvData.length; i++){
+            var csvRegion = csvData[i]; //the current region
+            var csvKey = csvRegion.adm1_code; //the CSV primary key
+
+            //loop through geojson regions to find correct region
+            for (var a=0; a<franceRegions.length; a++){
+
+                var geojsonProps = franceRegions[a].properties; //the current region geojson properties
+                var geojsonKey = geojsonProps.adm1_code; //the geojson primary key
+
+                //where primary keys match, transfer csv data to geojson properties object
+                if (geojsonKey == csvKey){
+
+                    //assign all attributes and values
+                    attrArray.forEach(function(attr){
+                        var val = parseFloat(csvRegion[attr]); //get csv attribute value
+                        geojsonProps[attr] = val; //assign attribute and value to geojson properties
+                    });
+                };
+            };
+
         //examine the results
         console.log(arizonaCounties);
         
     
+        };
     };
-
 };
